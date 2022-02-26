5edac999477d3d311aea47854e4d233f67d950a5
Nayan Mavani <nayanmavani16@gmail.com>
2022-02-26 23:09:33 +0100

    npm install

---
+++ landing-page/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Row } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Row></Row>
+     
+    </div>
+  );
+}
+
+export default App;
